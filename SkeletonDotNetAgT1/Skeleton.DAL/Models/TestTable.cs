337e192e11480877782ac2f4d319d00d41b78dde
Aron Szocs <arsz01@betssongroup.com>
2019-11-19 13:28:36 +0100

    Create model based on existing database

---
+++ SkeletonDotNetAgT1/Skeleton.DAL/Models/TestTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Skeleton.DAL.Models
+{
+    public partial class TestTable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? OrderNumber { get; set; }
+    }
+}
---
+++ SkeletonDotNetAgT1/Skeleton.DAL/SkeletonDatabaseContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Skeleton.DAL.Models;
+
+namespace Skeleton.DAL
+{
+    public partial class SkeletonDatabaseContext : DbContext
+    {
+        public SkeletonDatabaseContext()
+        {
+        }
+
+        public SkeletonDatabaseContext(DbContextOptions<SkeletonDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TestTable> TestTable { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=Skeleton.Database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestTable>(entity =>
+            {
+                entity.HasKey(e => e.Id)
+                    .HasName("PK_TESTTABLE_ID")
+                    .IsClustered(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(255);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
