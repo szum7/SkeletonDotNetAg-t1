cdba10ed4bd3b04f4f4b4069663d992d687c8e2f
Aron Szocs <arsz01@betssongroup.com>
2019-11-19 13:47:05 +0100

    add BL

---
+++ SkeletonDotNetAgT1/Skeleton.BL/TestTableRepository.cs
@@ -0,0 +1,25 @@
+﻿using Skeleton.DAL;
+using Skeleton.DAL.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Skeleton.BL
+{
+    public class TestTableRepository
+    {
+        public List<TestTable> Get()
+        {
+            using (var context = new SkeletonDatabaseContext())
+            {
+                return (from d in context.TestTable
+                        select new TestTable()
+                        {
+                            Id = d.Id,
+                            Name = d.Name,
+                            OrderNumber = d.OrderNumber
+                        }).ToList();
+            }
+        }
+    }
+}
